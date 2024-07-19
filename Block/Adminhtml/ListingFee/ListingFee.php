448d3f4f4919bee497d2637c29ba422d537d121c
Third Sugian <floranteIII.miranda.sugian@gmail.com>
2024-07-19 15:32:24 +0800

    add listing fee computation

--- Block/Adminhtml/ListingFee/ListingFee.php
+++ Block/Adminhtml/ListingFee/ListingFee.php
@@ -5,13 +5,15 @@
  */
 namespace Techstacks\Legacy\Block\Adminhtml\ListingFee;
 
+use DateTime;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Framework\View\Element\BlockInterface;
 use Magetop\Marketplace\Model\ResourceModel\Sellers\Collection;
 use Magetop\Marketplace\Model\ResourceModel\Sellers\CollectionFactory as SellerCollectionFactory;
-use Magetop\Marketplace\Model\ResourceModel\Products\Collection as ProductCollection;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as MageProductCollectionFactory;
 use Magetop\Marketplace\Model\ResourceModel\Products\CollectionFactory as ProductsCollectionFactory;
+use Psr\Log\LoggerInterface;
 
 class ListingFee extends Template implements BlockInterface
 {
@@ -31,20 +33,36 @@ class ListingFee extends Template implements BlockInterface
     protected $productsCollectionFactory;
 
     /**
+     * @var MageProductCollectionFactory
+     */
+    protected $mageProductCollectionFactory;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
      * @param Context $context
      * @param SellerCollectionFactory $sellersCollectionFactory
      * @param ProductsCollectionFactory $productsCollectionFactory
+     * @param MageProductCollectionFactory $mageProductCollectionFactory
+     * @param LoggerInterface $logger
      * @param array $data
      */
     public function __construct(
         Context $context,
         SellerCollectionFactory $sellersCollectionFactory,
         ProductsCollectionFactory $productsCollectionFactory,
+        MageProductCollectionFactory $mageProductCollectionFactory,
+        LoggerInterface $logger,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->sellersCollectionFactory = $sellersCollectionFactory;
         $this->productsCollectionFactory = $productsCollectionFactory;
+        $this->mageProductCollectionFactory = $mageProductCollectionFactory;
+        $this->logger = $logger;
     }
 
     /**
@@ -60,11 +78,64 @@ class ListingFee extends Template implements BlockInterface
     /**
      * @return int
      */
-    public function getProductsCollection($sellerId = '') {
+    public function getTotalActiveProductsBySellerId($sellerId = '') {
       return $this->productsCollectionFactory->create()
             ->addFieldToSelect('*')
             ->addFieldToFilter('status','1')
             ->addFieldToFilter('user_id', $sellerId)
             ->count();
+    }
+
+    /**
+     * @param $id
+     * @return \Magento\Catalog\Model\ResourceModel\Product\Collection
+     */
+    public function getPriceById($id)
+    {
+        $price = $this->mageProductCollectionFactory->create()
+            ->addAttributeToSelect('price')
+            ->addAttributeToSelect('created_at')
+            ->addAttributeToSelect('update_at')
+            ->addAttributeToFilter('entity_id', $id);
+
+        return $price->getFirstItem()->getData('price');
+    }
+
+    protected function isTwoMonthsAhead(DateTime $date): bool {
+        $currentDate = new DateTime();
+
+        $twoMonthsAhead = clone $currentDate;
+        $twoMonthsAhead->modify('+2 months');
+
+        return $date->format('Y-m') === $twoMonthsAhead->format('Y-m');
+    }
+
+    /**
+     * @param $sellerId
+     * @return float|int
+     */
+    public function getTotalListingFeeBySellerId($sellerId = '') {
+        try {
+            $productCollection = $this->productsCollectionFactory->create()
+                ->addFieldToSelect('*')
+                ->addFieldToFilter('status','1')
+                ->addFieldToFilter('user_id', $sellerId);
+
+            $priceArray = [];
+            foreach ($productCollection as $product) {
+                if($product->getData('created')) {
+                    $created = new \DateTime($product->getData('created'));
+                    if($this->isTwoMonthsAhead($created)) {
+                        $priceArray[] = (float)$this->getPriceById($product->getData('product_id')) * 0.03;
+                    } else {
+                        $priceArray[] = (float)$this->getPriceById($product->getData('product_id')) * 0.05;
+                    }
+                }
+            }
+            return array_sum($priceArray);
+        } catch (\Exception $e) {
+            $this->logger->warning($e->getMessage());
+            return 0;
+        }
     }
 }
